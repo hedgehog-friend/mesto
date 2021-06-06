f35536800dbf07688b3d37a8141fbc1aa47558a9
hedgehog-friend <66081601+hedgehog-friend@users.noreply.github.com>
2021-06-06 15:50:07 +0200

    рефактринг очистки формы

--- scripts/FormValidator.js
+++ scripts/FormValidator.js
@@ -78,11 +78,11 @@ class FormValidator {
 
   //чистка формы от ошибок при валидации на предыдущем открытии попапа
   prepareForm = () => {
-    this.#currentForm.querySelector(this.#submitButtonSelector).disabled = true;
-    const spanErrorList = Array.from(this.#currentForm.querySelectorAll('.popup__input-error'));
-    spanErrorList.forEach(element => element.classList.remove(this.#errorClass));
-    const inputErrorList = Array.from(this.#currentForm.querySelectorAll('.popup__form-item'));
-    inputErrorList.forEach(element => element.classList.remove(this.#inputErrorClass));
+    this.#buttonElement.disabled = true;
+    this.#inputList.forEach(inputElement => {
+      this.#hideInputError(inputElement)
+    });
+    this.#toggleButtonState();
   }
 
   enableValidation = () => {
